117de3cb1b110db284000f0d60b8064bda3f3a7a
Fujr <fjrcn@outlook.com>
2024-04-01 14:00:59 +0800

    feat 加入debug模式，显示推理时间、模型输入输出参数

--- src/Tensor.cpp
+++ src/Tensor.cpp
@@ -1,5 +1,6 @@
 #include "Tensor"
-Tensor::Tensor(void *model_char, int tensor_arena_size){
+Tensor::Tensor(void *model_char, int tensor_arena_size,bool DebugMode){
+    this->DebugMode = DebugMode;
     this->model_char = model_char;
     this->kTensorArenaSize = tensor_arena_size * 1024;
     _allocateTensors();
@@ -15,6 +16,7 @@ Tensor::~Tensor()
 
 void Tensor::infer()
 {
+    uint16_t start_time = millis();
     //print input.f
     // for (int i = 0; i < this->input->bytes / sizeof(float); i++){
     //     printf("input: %f\n", this->input->data.f[i]);
@@ -24,7 +26,10 @@ void Tensor::infer()
     {
         printf("Error invoking interpreter");
     }
-    memset(this->input->data.f, 0, this->input->bytes);
+    //memset(this->input->data.f, 0, this->input->bytes);
+    if (this->DebugMode){
+        printf("Inference time: %d\n", millis() - start_time);
+    }
 }
 
 void Tensor::_loadModel()
@@ -40,9 +45,22 @@ void Tensor::_loadModel()
     this->input = this->interpreter->input(0);
     this->output = this->interpreter->output(0);
     printf("Interpreter allocated");
+    if (this->DebugMode){
+        _debug();
+    }
 }
 
 void Tensor::_allocateTensors()
 {
     this->tensor_arena = (uint8_t *)heap_caps_malloc(this->kTensorArenaSize, MALLOC_CAP_SPIRAM);
+}
+
+
+void Tensor::_debug(){
+    //input dim ,input byte
+    printf("input dim: %d\n", this->input->bytes / sizeof(float));
+    printf("input byte: %d\n", this->input->bytes);
+    //output dim,output byte
+    printf("output dim: %d\n", this->output->bytes / sizeof(float));
+    printf("output byte: %d\n", this->output->bytes);
 }
