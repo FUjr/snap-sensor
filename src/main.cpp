8606baa14080d1e7f2374d041a4277090c967f66
Fujr <fjrcn@outlook.com>
2024-04-01 14:00:12 +0800

    feat: 使用从fs读取的配置初始化模型和特征提取类 在超过阈值时添加等待逻辑，避免重复检测

--- src/main.cpp
+++ src/main.cpp
@@ -15,48 +15,59 @@
 static HomeKit *homekit = nullptr;
 static Tensor *tensor = nullptr;
 static AudioProcessor *melSpectrogram;
+static float threshold;
 void setup()
 {
   Serial.begin(115200);
-  LoadModel loadmodel = LoadModel("/model_CNN.tflite");
-  char* model = new char[loadmodel.model_size];
-  memcpy(model, loadmodel.model, loadmodel.model_size);
   i2sPrepare::i2s_install(i2sPrepare::micModel::INMP441, I2S_NUM_0, 16000);
   i2sPrepare::i2s_set_pin(I2S_NUM_0, I2S_SCK, I2S_WS, I2S_SD);
-  tensor = new Tensor(model, 1024);
-  delete[] model;
-  //homekit = new HomeKit();
-  melSpectrogram = new AudioProcessor(1024,256,7680,16000,60,2000,6000,1);
+  homekit = new HomeKit();
+  LoadModel::ModelConfig loadmodel  = LoadModel::loadModelConfig("/model_CNN.tflite","/model_config.json");
+  threshold = loadmodel.threshold;
+  tensor = new Tensor(loadmodel.model, 1024, false);
+  melSpectrogram = new AudioProcessor(
+    loadmodel.fft_shift_length,
+    loadmodel.fft_step_length,
+    loadmodel.num_frames,
+    loadmodel.sample_rate,
+    loadmodel.num_mel_bins,
+    loadmodel.lower_frequency_limit,
+    loadmodel.upper_frequency_limit,
+    1
+    );
 }
 
 void loop()
 {
     unsigned long time_loop_start = millis();
     melSpectrogram->update();
+    unsigned long time_get_audio_feat = millis();
     for (int i = 0; i < melSpectrogram->audioFeature->nframes; i++)
     {
       for (int j = 0; j < melSpectrogram->audioFeature->num_mel_bins; j++)
       {
         tensor->input->data.f[i * melSpectrogram->audioFeature->nframes + j] = melSpectrogram->audioFeature->output_matrix[i][j];
-
       }
     }
+    unsigned long time_copy_audio = millis();
     tensor->infer();
+    unsigned long time_end_infering = millis();
     int output_length = tensor->output->bytes / sizeof(float);
     for (int j = 0; j < output_length; j++)
     {
       printf("output: %f\n", tensor->output->data.f[j]);
-      if (tensor->output->data.f[j] > 0.95)
+      if (tensor->output->data.f[j] > threshold)
       {
         if (homekit->switchEvent != nullptr)
         {
           homekit->switchEvent->setVal(0);
+          delay(512+ time_loop_start - time_end_infering );
         }
       }
     }
     unsigned long time_end = millis();
-    //printf("Time total: %d\n", time_end - time_loop_start);
-    // printf("Time get audio: %d\n", time_get_audio - time_loop_start);
-    // printf("Time compute: %d\n", time_compute - time_get_audio);
-    // printf("Time inferencing: %d\n", time_end - time_compute);
+    // printf("Time total: %d\n", time_end - time_loop_start);
+    // printf("Time get audio feat: %d\n", time_get_audio_feat - time_loop_start);
+    // printf("Time copy audio: %d\n", time_copy_audio - time_get_audio_feat);
+    // printf("Time inferencing: %d\n", time_end - time_copy_audio);
 }
